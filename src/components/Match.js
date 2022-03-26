6fb8d8a40d7a7ff2fe45bb062b775d0cba29c4e6
andyhuang9 <andyhuang159@gmail.com>
2022-03-26 11:39:04 -0700

    Added image input functionality

--- src/components/Match.js
+++ src/components/Match.js
@@ -5,16 +5,19 @@ import {
 	Text,
 	SafeAreaView,
 	Image, 
+	TouchableHighlight,
 } from 'react-native';
 
 function Match(props) {
 	return (
 		<SafeAreaView style={styles.match}>
+			<TouchableHighlight style={styles.avatarContainer}>
 			<Image 
 				style={styles.avatarStyle} 
 				resizeMode='center'
 				source={props.avatarFile}
 			/>
+			</TouchableHighlight>
 			<Text style={styles.name}>{props.matchName}</Text>
 			<Text></Text>
 		</SafeAreaView>
@@ -24,24 +27,39 @@ function Match(props) {
 
 const styles = StyleSheet.create({
 	match: {
-		backgroundColor: 'dodgerblue',
-		flex: 0.2,
+		backgroundColor: 'black',
+		flex: 0.13,
 		justifyContent: 'flex-start',
+		flexDirection: 'row',
+		width: '100%',
 	},
-	avatarStyle: {
+	avatarContainer: {
 		flex: 0.15,
-		height: 50,
 		width: 50,
+		height: 50,
 		borderRadius: 25,
 		marginLeft: 15,
-		justifyContent: 'center',
+		overflow: 'hidden',
+		alignSelf: 'center',
+	},
+	avatarStyle: {
+		flex: 1,
+		width: 100,
+		height: 100,
+		borderRadius: 50,
+		alignSelf: 'center',
+		resizeMode: 'contain',
 	},
 	name: {
-		flex: 0.1,
-		marginLeft: 15,
+		flex: 0.5,
+		marginLeft: 20,
 		fontWeight: 'bold',
-		fontSize: 10,
-		color: 'black',
+		fontSize: 15,
+		color: 'white',
+		alignSelf:'center',
+		// textShadowColor: "rgba(0, 0, 0, 0.75)",
+    	// textShadowOffset: { width: -1, height: 1 },
+    	// textShadowRadius: 15,
 	},
 })
 
