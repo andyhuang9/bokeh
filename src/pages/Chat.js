1e35d37c57fbfca02ba14844b947c6f41da81c97
andyhuang9 <andyhuang159@gmail.com>
2022-03-26 13:20:16 -0700

    Created variables for matches

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -2,14 +2,22 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import Match from '../components/Match';
 
-let gojo = require("../../assets/gojo.jpg");
-let lelouch = require("../../assets/lelouch.jpg");
+let gojo = {
+  name: "Gojo Satoru",
+  avatar: require("../../assets/gojo.jpg"),
+  notif: true,
+}
+let lelouch = {
+  name: "Lelouch vi Britannia",
+  avatar: require("../../assets/lelouch.jpg"),
+  notif: false,
+}
 
 function Chat() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start' }}>
-        <Match matchName="Gojo Satoru" avatarFile={gojo} notifBool={true}/>
-        <Match matchName="Lelouch vi Britannia" avatarFile={lelouch} notifBool={false}/>
+        <Match matchName={gojo.name} avatarFile={gojo.avatar} notifBool={gojo.notif}/>
+        <Match matchName={lelouch.name} avatarFile={lelouch.avatar} notifBool={lelouch.notif}/>
       </View>
     );
   }
