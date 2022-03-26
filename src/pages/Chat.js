fb3119edb5053faab4b4da93ab2cf30b568f2939
andyhuang9 <andyhuang159@gmail.com>
2022-03-26 13:13:29 -0700

    Added message windows to matches page

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import Match from '../components/Match';
+
+let gojo = require("../../assets/gojo.jpg");
+let lelouch = require("../../assets/lelouch.jpg");
 
 function Chat() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Chat Screen</Text>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start' }}>
+        <Match matchName="Gojo Satoru" avatarFile={gojo} notifBool={true}/>
+        <Match matchName="Lelouch vi Britannia" avatarFile={lelouch} notifBool={false}/>
       </View>
     );
   }
