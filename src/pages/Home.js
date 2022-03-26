fd27d3fdfd5e4e5520614b5990062b85c300912c
andyhuang9 <andyhuang159@gmail.com>
2022-03-26 15:36:03 -0700

    Changed home page to use index for card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,13 +3,12 @@ import { Button, View, Text } from 'react-native';
 import Card from '../components/Card';
 import users from '../../assets/users.js';
 
-
+let index = 0;
 
 function Home({navigation}) {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Card user={users[1]}/>
-
+          <Card user={users[index]}/>
         </View>
       );
   }
