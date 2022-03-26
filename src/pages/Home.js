1b0b702e37823ec57c1f08c9cf9e27a28e5ae014
andyhuang9 <andyhuang159@gmail.com>
2022-03-26 14:33:33 -0700

    Removed unnecessary imports

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,6 @@ import * as React from 'react';
 import { Button, View, Text } from 'react-native';
 import Card from '../components/Card';
 import users from '../../assets/users.js';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { FlatList } from 'react-native-web';
 
 
 
