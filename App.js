b75b933c926cfcced23833d0a006ed1f92b35343
Patrick Gousseau <patrickg@ensemble.com>
2022-03-26 17:17:20 -0700

    Added navigation for login

--- App.js
+++ App.js
@@ -6,18 +6,25 @@ import Home from './src/pages/Home'
 import Chat from './src/pages/Chat';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Header from './src/shared/Header'
+import Login from './src/pages/Login';
 
 
 const Stack = createBottomTabNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer initialRouteName="Login">
+     
       <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} options={{ title: 'Bokeh',  headerStyle: { backgroundColor: '#FFA500',}}} />
         <Stack.Screen name="Home" component={Home} options={{ title: 'Bokeh',  headerStyle: { backgroundColor: '#FFA500',}}} />
         <Stack.Screen name="Chat" component={Chat} options={{ title: 'Bokeh',  headerStyle: { backgroundColor: '#FFA500',}}}/>
       </Stack.Navigator>
     </NavigationContainer>
+
+    
+
+    
 
   );
 }
---
+++ src/pages/Login.js
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { View, Text, StatusBar, StyleSheet, TextInput,TouchableOpacity, Image } from 'react-native';
+
+function Login({navigation}) {
+   
+    return (
+      <View style={styles.container}>
+
+        <Image style={styles.image} source={require("../../assets/logo-nobackground.png")} />
+
+        <TouchableOpacity style={styles.loginBtn} onPress={() =>
+        navigation.navigate('Home')}>
+          <Text style={styles.loginText}>LOGIN</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+  () => {<component></component>}
+   
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+   
+    image: {
+
+      margin: 40,
+    },
+   
+    loginBtn: {
+      width: "80%",
+      borderRadius: 25,
+      height: 50,
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 40,
+      backgroundColor: "#FFA500",
+    },
+  });
+  
+
+export default Login;
